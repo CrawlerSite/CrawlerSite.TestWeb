f1ddc00fabc54d590b5a10723c5d4a76da6f5af2
Cami Rodriguez <camiepisode@outlook.com>
2017-04-06 21:48:28 -0500

    improved code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-var express         = require("express");
-var app             = express();
-var path            = require('path');
-var http            = require('http').Server(app);
-var fs              = require('fs');
-var bodyParser      = require('body-parser');
-var cors            = require('cors');
-var open            = require('open');
+var express = require("express");
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var open = require('open');
 
 var port = 3501;
 
@@ -17,14 +17,32 @@ app.use(cors());
 
 
 app.engine('html', require('ejs').renderFile);
-        app.set("view options", {layout: false});
-        
-        app.use(express.static(path.join(__dirname, ''))); //  "public" off of current is root
-        
-        app.get('/', function(req, res){
-            res.render("index.html");
-        });
+app.set("view options", { layout: false });
 
-app.listen(process.env.PORT);
+app.use(express.static(path.join(__dirname, ''))); //  "public" off of current is root
 
-open('http://localhost:' + process.env.PORT);
+app.get('/', function (req, res) {
+    res.render("index.html");
+});
+
+var normalizePort = function (val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+var normalizedPort = normalizePort(process.env.PORT || port);
+
+app.listen(normalizedPort);
+
+open('http://localhost:' + normalizedPort);
